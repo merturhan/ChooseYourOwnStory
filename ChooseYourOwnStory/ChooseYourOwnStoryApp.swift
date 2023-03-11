3cafd79add163325faab2bcfdb0d7b7353d734d3
Mert Urhan <mert-urhan@hotmail.com>
2023-03-11 16:06:23 +0300

    Initial Commit

---
+++ ChooseYourOwnStory/ChooseYourOwnStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYourOwnStoryApp.swift
+//  ChooseYourOwnStory
+//
+//  Created by Mert Urhan on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYourOwnStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
